07154598834506afc45f3c2acb0d11a183217d3a
yonggaochao <gcy2014@126.com>
2020-05-06 15:31:46 +0800

    删除无用文件

--- version.js
+++ version.js
@@ -1,4 +1,4 @@
-var sensorsdata_js_sdk_latest_version = '1.15.1';
+var baize_js_sdk_latest_version = '1.15.1';
 if(typeof module ==='object' && typeof module.exports === 'object'){
-module.exports = sensorsdata_js_sdk_latest_version;
+module.exports = baize_js_sdk_latest_version;
 }
